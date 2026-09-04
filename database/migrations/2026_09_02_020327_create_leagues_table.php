44478b18af9bb7108b4d9778ab51b0bf929219da
shimizushoya <simitti560517@gmail.com>
2026-09-04 11:56:34 +0900

    feat: leaguesテーブルのマイグレーション作成

--- database/migrations/2026_09_02_020327_create_leagues_table.php
+++ database/migrations/2026_09_02_020327_create_leagues_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('country');
-            $table->integer('api_id');
+            $table->integer('api_id')->unique();
             $table->timestamps();
         });
     }
