487f07b7e2edbef5a30cab553ba88309e38b60c6
shimizushoya <simitti560517@gmail.com>
2026-09-08 13:47:48 +0900

    wip: matchesテーブルとMatchStatus Enumを実装中

--- database/migrations/2026_09_02_020512_create_clubs_table.php
+++ database/migrations/2026_09_02_020512_create_clubs_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('clubs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('league_id')->constrained();
+            $table->string('name');
+            $table->integer('api_id')->unique();
+            $table->string('logo_url')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2026_09_02_020539_create_matches_table.php
+++ database/migrations/2026_09_02_020539_create_matches_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\MatchStatus;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('matches', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('league_id')->constrained();
+            $table->foreignId('home_club_id')->constrained('clubs');
+            $table->foreignId('away_club_id')->constrained('clubs');
+            $table->enum('match_status', array_column(MatchStatus::cases(), 'value'));
             $table->timestamps();
         });
     }
