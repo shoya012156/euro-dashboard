beceeb6779fc3e40abda5ff3cdf23c2d3f929375
shimizushoya <simitti560517@gmail.com>
2026-09-10 13:27:50 +0900

    feat: LeagueSeederに6テーブル分のデータを追加

---
+++ database/seeders/LeagueSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LeagueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('leagues')->delete();
+        DB::table('leagues')->insert([
+            [
+                'name' => 'Premier League',
+                'country' => 'England',
+                'api_id' => 2021,
+            ],
+            [
+                'name' => 'Primera Division',
+                'country' => 'Spain',
+                'api_id' => 2014,
+            ],
+            [
+                'name' => 'Bundesliga',
+                'country' => 'Germany',
+                'api_id' => 2002,
+            ],
+            [
+                'name' => 'Serie A',
+                'country' => 'Italy',
+                'api_id' => 2019,
+            ],
+            [
+                'name' => 'Ligue 1',
+                'country' => 'France',
+                'api_id' => 2015,
+            ],
+            [
+                'name' => 'Eredivisie',
+                'country' => 'Netherlands',
+                'api_id' => 2003,
+            ],
+        ]);
+    }
+}
