00a80d61b31d4124aca7530e21427bfcc6ad800b
shimizushoya <simitti560517@gmail.com>
2026-09-10 10:31:03 +0900

    feat: playersテーブルを実装

--- database/migrations/2026_09_02_022728_create_players_table.php
+++ database/migrations/2026_09_02_022728_create_players_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('club_id')->constrained();
+            $table->integer('api_id')->unique();
             $table->timestamps();
         });
     }
